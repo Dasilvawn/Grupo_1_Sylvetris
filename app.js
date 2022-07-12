4a98271e691914e495210b101662da6f363015f5
hernan0066 <hernan0066@gmail.com>
2022-07-12 17:03:08 -0300

    Server init and structure file

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
+
+app.use(express.static('public'));
+
+//rutas
+app.get('/', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'index.html'));});
+app.get('/carrito', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'productCart.html'));});
+app.get('/producto', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'productDetail.html'));});
+app.get('/login', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'login.html'));});
+app.get('/register', (req, res) => {res.sendFile( path.join(__dirname, 'views', 'register.html'));});
+
+//iniciar servidor
+app.listen(port, () => {
+    console.log( `Servidor iniciado en puerto ${port}` );
+});
