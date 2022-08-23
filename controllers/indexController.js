19da1205972ecfc949be5aa89921b5ddea2db911
hernan0066 <hernan0066@gmail.com>
2022-08-23 12:32:41 -0300

    Add search view

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
-const {categorias,instagram} = require('../data/data')
+const { categorias, instagram } = require("../data/data");
 const fs = require("fs");
 const path = require("path");
-const { formatPrice } = require('../utils/moneda');
+const { formatPrice } = require("../utils/moneda");
 
 const productsFilePath = path.join(__dirname, "../data/products.json");
 
@@ -9,14 +9,33 @@ module.exports = {
   index: (req, res) => {
     const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
     const productsByFeatured = products.filter((product) => product.destacado);
-    
+
     return res.render("index", {
-      title: "Sylvestris | Home", 
+      title: "Sylvestris | Home",
       categorias,
 
       productsByFeatured,
       formatPrice,
       instagram,
+    });
+  },
+  search: (req, res) => {
+    let { keywords } = req.query;
+    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+    const productsByFeatured = products.filter((product) => product.destacado);
+
+    let result = products.filter((product) =>
+      product.nombre.toLowerCase().includes(keywords.toLowerCase() ) || product.categoria.toLowerCase().includes(keywords.toLowerCase() )
+    );
+    //console.log(result);
+    //return res.send(result)
+
+    return res.render("./products/search", {
+      title: "Sylvestris | Search",
+      keywords,
+      result,
+      formatPrice,
+      productsByFeatured
     });
   },
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { index } = require('../controllers/indexController');
+const { index, search } = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/', index);
+router.get('/search', search)
+
 
 module.exports = router;
