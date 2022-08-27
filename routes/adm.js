e500c27bda1440e7592630628da4784337017d6c
hernan0066 <hernan0066@gmail.com>
2022-08-27 16:04:31 -0300

    Add user in admin dashboard

--- routes/adm.js
+++ routes/adm.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const admController = require("../controllers/admController");
 const router = express.Router();
-const {dashboard,  store, edit, update, createProduct, getProducts, deleteProducts } = require("../controllers/admController");
-
+const {
+  getUsers,
+  dashboard,
+  store,
+  edit,
+  update,
+  createProduct,
+  getProducts,
+  deleteProducts,
+} = require("../controllers/admController");
 
 /* /admin */
-
 
 router.get("/", dashboard);
 
 router.get("/createProduct", createProduct);
 router.get("/products", getProducts);
 
-/*** editar productos***/ 
-router.get('/editProduct/:id', edit); 
-router.put('/update/:id', update); 
+/*** editar productos***/
+router.get("/editProduct/:id", edit);
+router.put("/update/:id", update);
 
 /* Formulario de creación de productos */
-router.post('/store', store); 
+router.post("/store", store);
 
 router.delete("/deleteProduct/:id", deleteProducts);
+
+//users
+router.get("/users", getUsers);
 
 module.exports = router;
