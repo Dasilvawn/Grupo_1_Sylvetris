66defaadb9211da3cca634549f9861683e64779d
YR-Johnson <eljonybarra@gmail.com>
2022-10-15 02:59:40 -0300

    modified routes products.js and users.js, modified validations addressChangeValidator.js, editUserProfileValidator.js and passwordChangeValidator.js, I modified the views in products (categories, products and search) and users (address, changue_address, changue_password, profile and rename)

--- validations/passwordChangeValidator.js
+++ validations/passwordChangeValidator.js
@@ -1,30 +1,41 @@
-const {check, body} = require('express-validator');
-const users = require('../data/db').loadUsers();
-const bcryptjs = require('bcryptjs');
+const { check, body } = require("express-validator");
+const users = require("../data/db").loadUsers();
+const bcryptjs = require("bcryptjs");
+const db = require("../database/models");
 
 module.exports = [
-    
-    body('password_old')
-        .notEmpty().withMessage('*Requerido').bail()
-        .custom((value, {req}) => {
-            let user = users.find(user =>  bcryptjs.compareSync(value,user.password));
-            return !!user
-        }).withMessage('*Contraseña incorrecta!'),
-    check('password')
-        .notEmpty().withMessage('*Requerido').bail()
-        .isLength({
-            min : 6,
-            max : 12
-        }).withMessage('La contraseña debe tener entre 6 y 12 caracteres'),
-    body('password2')
-        .notEmpty().withMessage('*Requerido').bail()
-        .custom((value,{req}) => {
-            if(value !== req.body.password ){
-                return false
-            }else{
-                return true
-            }
-        }).withMessage('Las contraseñas no coinciden'),
-    
-  
-]
+  body("password_old")
+    .notEmpty()
+    .withMessage("*Requerido")
+    .bail()
+    .custom((value, { req }) => {
+      /*  let user = users.find(user =>  bcryptjs.compareSync(value,user.password));
+            return !!user */
+      return db.User.findByPk(req.session.userLogin.id).then((user) => {
+        if (!bcryptjs.compareSync(value, user.password)) {
+          return Promise.reject("*Contraseña incorrecta!");
+        }
+      });
+    }),
+  check("password")
+    .notEmpty()
+    .withMessage("*Requerido")
+    .bail()
+    .isLength({
+      min: 6,
+      max: 12,
+    })
+    .withMessage("La contraseña debe tener entre 6 y 12 caracteres"),
+  body("password2")
+    .notEmpty()
+    .withMessage("*Requerido")
+    .bail()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        return false;
+      } else {
+        return true;
+      }
+    })
+    .withMessage("Las contraseñas no coinciden"),
+];
