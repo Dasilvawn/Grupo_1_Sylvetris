1b76633e17aa5a90f013d0eb8c9e17bfe7df66bf
hernan0066 <hernan0066@gmail.com>
2022-11-01 09:05:26 -0300

    Fix config

--- database/config/config.js
+++ database/config/config.js
@@ -1,21 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-  /* development: {
+  development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "mysql",
-  }, */
-  development: {
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    dialect: "mysql",
-  },
+  }, 
+  
   test: {
     username: "root",
     password: "",
