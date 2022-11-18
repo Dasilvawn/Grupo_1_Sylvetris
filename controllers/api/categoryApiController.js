e8d9ccb189578f3546caffd123fe128dad9c4b65
hernan0066 <hernan0066@gmail.com>
2022-11-18 17:17:50 -0300

    Fix link router

--- controllers/api/categoryApiController.js
+++ controllers/api/categoryApiController.js
@@ -1,61 +1,9 @@
 const { validationResult } = require("express-validator");
 const db = require("../../database/models");
-const bcryptjs = require("bcryptjs");
-const path = require("path");
-const fs = require("fs");
 
 const getCategoriesApi = (req, res) => {
-    const id = req.session.userLogin?.id;
-    let categories = db.Category.findAll();
-    let user = db.User.findByPk(id);
-
-    Promise.all([categories, user])
-      .then(([categories, user]) => {
-        return res.status(200).json({
-            meta: {
-              ok: true,
-              status: 200,
-              count: categories.length,
-            },
-            data: {
-                categories,
-            },
-          });
-      })
-      .catch((error) => console.log(error));
-  };
-const getCreateCategory = (req, res) => {
-    const id = req.session.userLogin?.id;
-    let categories = db.Category.findAll();
-    let user = db.User.findByPk(id);
-
-    Promise.all([categories, user])
-      .then(([categories, user]) => {
-        return res.status(200).json({
-            meta: {
-              ok: true,
-              status: 200,
-              count: categories.length,
-            },
-            data: {
-                categories,
-            },
-          });
-      })
-      .catch((error) => console.log(error));
-  };
-const postCreateCategoryApi = (req, res) => {
-    let errors = validationResult(req);
-
-    if (errors.isEmpty()) {
-      db.Category.create({
-        name: req.body.name.trim(),
-      })
-        .then((category) => {
-          res.redirect("/admin/categories");
-        })
-        .catch((error) => console.log(error));
-    } else {
+  db.Category.findAll()
+    .then(([categories]) => {
       return res.status(200).json({
         meta: {
           ok: true,
@@ -63,77 +11,122 @@ const postCreateCategoryApi = (req, res) => {
           count: categories.length,
         },
         data: {
-            errors: errors.mapped(),
-            old: req.body,
+          categories,
         },
       });
-    }
-  };
-const getEditCategoryApi = (req, res) => {
-    const id = req.session.userLogin?.id;
-    let category = db.Category.findByPk(req.params.id);
-    let user = db.User.findByPk(id);
+    })
+    .catch((error) => console.log(error));
+};
+const getCreateCategory = (req, res) => {
+  const id = req.session.userLogin?.id;
+  let categories = db.Category.findAll();
+  let user = db.User.findByPk(id);
 
-    Promise.all([category, user])
-      .then(([category, user]) => {
-        return res.status(200).json({
-            meta: {
-              ok: true,
-              status: 200,
-              count: categories.length,
-            },
-            data: {
-                category,
-            },
-          });
+  Promise.all([categories, user])
+    .then(([categories, user]) => {
+      return res.status(200).json({
+        meta: {
+          ok: true,
+          status: 200,
+          count: categories.length,
+        },
+        data: {
+          categories,
+        },
+      });
+    })
+    .catch((error) => console.log(error));
+};
+const postCreateCategoryApi = (req, res) => {
+  let errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    db.Category.create({
+      name: req.body.name.trim(),
+    })
+      .then((category) => {
+        res.redirect("/admin/categories");
       })
       .catch((error) => console.log(error));
-  };
+  } else {
+    return res.status(200).json({
+      meta: {
+        ok: true,
+        status: 200,
+        count: categories.length,
+      },
+      data: {
+        errors: errors.mapped(),
+        old: req.body,
+      },
+    });
+  }
+};
+const getEditCategoryApi = (req, res) => {
+  const id = req.session.userLogin?.id;
+  let category = db.Category.findByPk(req.params.id);
+  let user = db.User.findByPk(id);
+
+  Promise.all([category, user])
+    .then(([category, user]) => {
+      return res.status(200).json({
+        meta: {
+          ok: true,
+          status: 200,
+          count: categories.length,
+        },
+        data: {
+          category,
+        },
+      });
+    })
+    .catch((error) => console.log(error));
+};
 
 const putEditCategoryApi = (req, res) => {
-    let errors = validationResult(req);
+  let errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-      db.Category.update(
-        {
-          name: req.body.name.trim(),
+  if (errors.isEmpty()) {
+    db.Category.update(
+      {
+        name: req.body.name.trim(),
+      },
+      {
+        where: {
+          id: req.params.id,
         },
-        {
-          where: {
-            id: req.params.id,
-          },
-        }
-      )
-        .then((category) => {
-          res.redirect("/admin/categories");
-        })
-        .catch((error) => console.log(error));
-    } else {
-      return res.status(200).json({
-        meta: {
-          ok: true,
-          status: 200,
-          count: categories.length,
-        },
-        data: {
-            errors: errors.mapped(),
-            old: req.body,
-        },
-      });
-    }
-  };
-const deleteCategoryApi = (req, res) => {
-    db.Category.destroy({
-      where: { id: req.params.id },
+      }
+    )
+      .then((category) => {
+        res.redirect("/admin/categories");
+      })
+      .catch((error) => console.log(error));
+  } else {
+    return res.status(200).json({
+      meta: {
+        ok: true,
+        status: 200,
+        count: categories.length,
+      },
+      data: {
+        errors: errors.mapped(),
+        old: req.body,
+      },
     });
-    res.redirect("/admin/categories");
-  };
-  
-  module.exports = {
-    getCategoriesApi,
-    getCreateCategory,
-    postCreateCategoryApi,
-    getEditCategoryApi,
-    putEditCategoryApi,
-    deleteCategoryApi,
-  };
+  }
+};
+const deleteCategoryApi = (req, res) => {
+  db.Category.destroy({
+    where: { id: req.params.id },
+  });
+  res.redirect("/admin/categories");
+};
+
+module.exports = {
+  getCategoriesApi,
+  getCreateCategory,
+  postCreateCategoryApi,
+  getEditCategoryApi,
+  putEditCategoryApi,
+  deleteCategoryApi,
+};
--- routes/category-api.js
+++ routes/category-api.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const { getCategoriesApi,
-        getCreateCategory,
-        postCreateCategoryApi,
-        getEditCategoryApi,
-        putEditCategoryApi,
-        deleteCategoryApi } = require("../controllers/api/categoryApiController");
+const {
+  getCategoriesApi,
+  getCreateCategory,
+  postCreateCategoryApi,
+  putEditCategoryApi,
+  deleteCategoryApi,
+} = require("../controllers/api/categoryApiController");
 const adminUserCheck = require("../middlewares/adminUserCheck");
-const { errorsCheck } = require("../middlewares/errorsCheck");
-const { tokenCheck } = require("../middlewares/tokenCheck");
-const { uploadImageAvatar } = require("../middlewares/uploadFile");
 
-//api/users
-router.get("/" ,tokenCheck, getCategoriesApi);
-router.get("/:id",tokenCheck ,getCreateCategory);
-router.post("/",tokenCheck, uploadImageAvatar.array("avatar", 1), postCreateCategoryApi);
-router.put("/:id", tokenCheck, putEditCategoryApi);
-router.delete("/:id", tokenCheck, deleteCategoryApi);
+const { tokenCheck } = require("../middlewares/tokenCheck");
+
+//api/category
+router.get("/", getCategoriesApi);
+router.get("/:id", getCreateCategory);
+router.post("/", tokenCheck, adminUserCheck, postCreateCategoryApi);
+router.put("/:id", tokenCheck, adminUserCheck, putEditCategoryApi);
+router.delete("/:id", tokenCheck, adminUserCheck, deleteCategoryApi);
 
 module.exports = router;
