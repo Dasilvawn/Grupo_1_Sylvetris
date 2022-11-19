6ec8c9ad38e8e1a9ae8e773417fe273010225e00
Walter <dasilvawn@gmail.com>
2022-11-19 10:51:10 -0300

    add  postApiProduct

--- controllers/api/productsApiController.js
+++ controllers/api/productsApiController.js
@@ -30,7 +30,13 @@ const newUserValidator = require("../../validations/newUserValidator");
   //  return res.send('geApiProducts')
   try { 
     const product = await db.Product.findByPk(req.params.id, {      
-      attributes : ['id','nombre','sub_titulo','slug','stock','destacado','descripcion','precio','cuidados'],
+      attributes : {
+        exclude:[
+            'createdAt',
+            'updatedAt',
+            'deletedAt'
+        ],
+      },
     });
      res.status(200).json({
       meta: {
@@ -51,12 +57,63 @@ const newUserValidator = require("../../validations/newUserValidator");
   }  
    
   }
+
   const postApiProduct  = async (req, res) =>{
    // return res. send(req.body)
-   
+   try {
+     const {
+      id,
+      nombre,
+      sub_titulo,
+      slug,
+      stock,
+      destacado,
+      descripcion,
+      precio,
+      cuidados
+    } = req.body;
+
+      
+     let newProduct = await db.Product.create({
+      id: id,
+      nombre: nombre.trim(),
+      sub_titulo: sub_titulo.trim(),
+      slug: slug,
+      stock: stock?stock:null,
+      destacado: destacado?destacado:null,
+      descripcion:descripcion.trim(),
+      precio: precio?precio:null,
+      cuidados: cuidados.trim(),
+     });
+
+     res.render(200).json({
+      meta:{
+        ok: true,
+        status: 200,
+        url: '/api/products'
+      },
+      date:{
+      product: {id: newProduct.id}
+     }  
+   });
+
+   } catch (error) {
+    return res.status(500).json({
+      ok:false,
+      status: 500,
+      msg: 'Comuniquese con el Administrador'
+    });
+   }
+
   }
   const putApiProduct  = async (req, res) =>{
     // return res.send('put')
+    try {
+      
+      
+    } catch (error) {
+      
+    }
   }
   const deleteApiProduct  = async (req, res) =>{
     return res.send('delete')
