e7f3918377b62c350aa96aa15a9823e30983d999
Walter <dasilvawn@gmail.com>
2022-11-18 23:40:03 -0300

    change apiProducts

--- controllers/api/productsApiController.js
+++ controllers/api/productsApiController.js
@@ -3,22 +3,70 @@ const bcryptjs = require("bcryptjs");
 const newUserValidator = require("../../validations/newUserValidator");
 
   const getApiProducts  = async (req, res) =>{
+   
 
+    try {
+   
+      const product = await db.Product.findAll({
+        
+            attributes: ['id', 'name', 'lastname', 'email']
+      });
+  
+      const productsResp = product.map(product => {
+        return {
+          ...product.dataValues,
+          urlData: `${req.protocol}://${req.get("host")}${req.baseUrl}/${product.id}`,
+        }
+      })
+  
+      return res.status(200).json({
+        meta: {
+          ok: true,
+          status: 200,
+          count: users.length
+        },
+        data: {
+          
+        products: userResp
+        },
+      });
+    } catch (error) {
+      return res.status(200).json({
+        meta: {
+          ok: false,
+          status: 500,
+          msg: error.message,
+        },
+      });
+    }
+    // try{
+    //     const products = await db.products.findAll({
+
+    //       attributes: ['id', 'name', 'lastname', 'email']
+    //     });
+
+    //     return res.send('holalala')
+          
+    //   }catch (err){
+    //     console.log(err)
+    //   }
+       
     
   }
-
-   
+  
   const getApiProduct  = async (req, res) =>{
-
+  //  return res.send('geApiProducts')
+   
   }
   const postApiProduct  = async (req, res) =>{
-
+  
+    // return res. send(req.body)
   }
   const putApiProduct  = async (req, res) =>{
-
+    // return res.send('put')
   }
   const deleteApiProduct  = async (req, res) =>{
-
+    return res.send('delete')
   } 
 
 
--- routes/products-api.js
+++ routes/products-api.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const {getApiProducts, getApiProduct, postApiProduct, putApiProduct, deleteApiProduct } = require("../controllers/api/productsApiController");
-const adminUserCheck = require("../middlewares/adminUserCheck");
-const { errorsCheck } = require("../middlewares/errorsCheck");
-const { tokenCheck } = require("../middlewares/tokenCheck");
-const { uploadImageAvatar } = require("../middlewares/uploadFile");
+
 
 //api/products
 router.get("/" , getApiProducts);
