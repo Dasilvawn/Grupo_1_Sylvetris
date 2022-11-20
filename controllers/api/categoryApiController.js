1d625aac8cf04360c3ae65fc3c68632a39cab980
Walter <dasilvawn@gmail.com>
2022-11-20 17:40:45 -0300

    modified:   app.js         modified:   controllers/api/categoryApiController.js         modified:   routes/category-api.js

--- controllers/api/categoryApiController.js
+++ controllers/api/categoryApiController.js
@@ -4,7 +4,57 @@ const bcryptjs = require("bcryptjs");
 const path = require("path");
 const fs = require("fs");
 
-const getCategoriesApi = (req, res) => {
+const getCategoriesApi = async (req, res) => {
+      
+ try {
+  const categories = await db.Category.findAll({
+      
+    attributes: ['id','name']
+});
+
+return res.status(200).json({
+meta: {
+  ok: true,
+  status: 200,
+  count: categories.length
+},
+data: {
+  categories
+},
+});
+  
+ } catch (error) {
+  return res.status(500).json({
+    meta: {
+      ok: false,
+      status: 500,
+      msg: error.message,
+    },
+  });
+ }
+};
+
+    // const id = req.session.userLogin?.id;
+    // let categories = db.Category.findAll();
+    // let user = db.User.findByPk(id); git 
+
+    // Promise.all([categories, user])
+    //   .then(([categories, user]) => {
+    //     return res.status(200).json({
+    //         meta: {
+    //           ok: true,
+    //           status: 200,
+    //           count: categories.length,
+    //         },
+    //         data: {
+    //             categories,
+    //         },
+    //       });
+    //   })
+    //   .catch((error) => console.log(error));
+       
+
+const getCategoryApi = (req, res) => {
     const id = req.session.userLogin?.id;
     let categories = db.Category.findAll();
     let user = db.User.findByPk(id);
@@ -24,26 +74,7 @@ const getCategoriesApi = (req, res) => {
       })
       .catch((error) => console.log(error));
   };
-const getCreateCategory = (req, res) => {
-    const id = req.session.userLogin?.id;
-    let categories = db.Category.findAll();
-    let user = db.User.findByPk(id);
 
-    Promise.all([categories, user])
-      .then(([categories, user]) => {
-        return res.status(200).json({
-            meta: {
-              ok: true,
-              status: 200,
-              count: categories.length,
-            },
-            data: {
-                categories,
-            },
-          });
-      })
-      .catch((error) => console.log(error));
-  };
 const postCreateCategoryApi = (req, res) => {
     let errors = validationResult(req);
 
@@ -68,26 +99,6 @@ const postCreateCategoryApi = (req, res) => {
         },
       });
     }
-  };
-const getEditCategoryApi = (req, res) => {
-    const id = req.session.userLogin?.id;
-    let category = db.Category.findByPk(req.params.id);
-    let user = db.User.findByPk(id);
-
-    Promise.all([category, user])
-      .then(([category, user]) => {
-        return res.status(200).json({
-            meta: {
-              ok: true,
-              status: 200,
-              count: categories.length,
-            },
-            data: {
-                category,
-            },
-          });
-      })
-      .catch((error) => console.log(error));
   };
 
 const putEditCategoryApi = (req, res) => {
@@ -122,6 +133,7 @@ const putEditCategoryApi = (req, res) => {
       });
     }
   };
+
 const deleteCategoryApi = (req, res) => {
     db.Category.destroy({
       where: { id: req.params.id },
@@ -131,9 +143,8 @@ const deleteCategoryApi = (req, res) => {
   
   module.exports = {
     getCategoriesApi,
-    getCreateCategory,
+    getCategoryApi,
     postCreateCategoryApi,
-    getEditCategoryApi,
     putEditCategoryApi,
     deleteCategoryApi,
   };
--- routes/category-api.js
+++ routes/category-api.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getCategoriesApi,
-        getCreateCategory,
-        postCreateCategoryApi,
-        getEditCategoryApi,
-        putEditCategoryApi,
-        deleteCategoryApi } = require("../controllers/api/categoryApiController");
+const { getCategoriesApi, getCategoryApi, postCreateCategoryApi,  putEditCategoryApi,  deleteCategoryApi} = require("../controllers/api/categoryApiController");
 const adminUserCheck = require("../middlewares/adminUserCheck");
 const { errorsCheck } = require("../middlewares/errorsCheck");
 const { tokenCheck } = require("../middlewares/tokenCheck");
 const { uploadImageAvatar } = require("../middlewares/uploadFile");
 
-//api/users
-router.get("/" ,tokenCheck, getCategoriesApi);
-router.get("/:id",tokenCheck ,getCreateCategory);
+// api/category
+router.get("/" ,tokenCheck, getCategoriesApi,); //funciona sin el tocken
+router.get("/:id",tokenCheck ,getCategoryApi);
 router.post("/",tokenCheck, uploadImageAvatar.array("avatar", 1), postCreateCategoryApi);
-router.put("/:id", tokenCheck, putEditCategoryApi);
-router.delete("/:id", tokenCheck, deleteCategoryApi);
+router.put("/:id", tokenCheck,  putEditCategoryApi);
+router.delete("/:id", tokenCheck,  deleteCategoryApi);
 
 module.exports = router;
