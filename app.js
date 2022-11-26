62bf4b60213830d6c2fff11334a13a1209cc9fc0
hernan0066 <hernan0066@gmail.com>
2022-11-25 21:20:38 -0300

    Add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
+var cors = require('cors')
 
 var passport = require("passport");
 
@@ -35,6 +36,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors())
 
 app.use(methodOverride("_method"));
 app.use(
